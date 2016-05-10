87b712712f7b44536611fd354a047b6821fa5527
Tanyabel <c1961685@trbvn.com>
2016-05-11 00:51:34 +0300

    Реализован "осторожный" алгоритм

--- www/js/algorithms.js
+++ www/js/algorithms.js
@@ -1,107 +1,85 @@
 Algorithms = (function() {
-	var	nodes = []; 
-	var neighbors = [];
-	var graph;
-
-	var initGrapth = function(_graph){
-		graph = _graph;
-		$.each(graph, function(index, node) {
-			nodes.push(node.id);
-			neighbors[node.id] = node.neighbors.slice();
-		});
-	};
-
-	var findCommunity = function(_graph){
-		if(!graph)
-			initGrapth(_graph);
-
-		var m = 0;
-		$.each(nodes, function(index, node) {
-			m += neighbors[node] ? neighbors[node].length : 0;
-		});
-		m /= 2;
-
-		var a = {};
-		$.each(nodes, function(index, node) {
-			a[node] = neighbors[node] ? neighbors[node].length / (2 * m) : 0;
-		});
-
-		var deltaQ = {};		
-		$.each(nodes, function(i, nodeI){
-			deltaQ[nodeI] = {nodes: [nodeI]};
-			$.each(nodes, function(j, nodeJ){
-				deltaQ[nodeI][nodeJ] = (neighbors[nodeI].indexOf(nodeJ) != -1) ? 1/(2*m) - a[nodeI] * a[nodeJ] : 0;				
-			});			
-		});
-
-		while (true) {
-			var maxI = -1;
-			var maxJ = -1;
-			var maxDeltaQ = 0;
-			for(var nodeI in deltaQ) {
-				nodeI = parseInt(nodeI);
-				for(var nodeJ in deltaQ){					
-					nodeJ = parseInt(nodeJ);
-					if(nodeI == nodeJ)
-						continue;
-
-					if(deltaQ[nodeI][nodeJ] >= maxDeltaQ){
-						maxDeltaQ = deltaQ[nodeI][nodeJ];
-						maxI = nodeI;
-						maxJ = nodeJ;
-					}
-				}
-   			}
-
-   			if(maxDeltaQ <= 0 || Object.keys(deltaQ).length === 1)
-   				break;
-
-   			for(var k in deltaQ) {
-   				k = parseInt(k);
-   				if( k == maxI || k == maxJ)
-   					continue;
-
-   				var isNeighborsWithI = isNeighbors(deltaQ[maxI].nodes, deltaQ[k].nodes);
-   				var isNeighborsWithJ = isNeighbors(deltaQ[maxJ].nodes, deltaQ[k].nodes);
-
-				deltaQ[maxJ][k] =  isNeighborsWithI && isNeighborsWithJ
-					? deltaQ[maxI][k] + deltaQ[maxJ][k]
-					: isNeighborsWithI && !isNeighborsWithJ
-						? deltaQ[maxI][k] - 2 * a[maxJ] * a[k]
-						: isNeighborsWithJ && !isNeighborsWithI ? deltaQ[maxJ][k] - 2 * a[maxI] * a[k] : 0;
-
-				deltaQ[k][maxJ] = deltaQ[maxJ][k];
-				var row = deltaQ[k];
-				delete row[maxI];
-   			}
-   			deltaQ[maxJ].nodes = deltaQ[maxJ].nodes.concat(deltaQ[maxI].nodes);   			
-   			delete deltaQ[maxJ][maxI];
-			delete deltaQ[maxI];
-			a[maxJ] = a[maxJ] + a[maxI];
-			a[maxI] = 0;			
-		}
-
-		var communities = [];
-		for(var item in deltaQ) {
-			communities.push(deltaQ[item].nodes);
-		};
-
-		return communities;		
-	};
-
-	var isNeighbors = function(source, targer) {
-		var res = false;
-		$.each(source, function(index, sourceNode) {
-			if (_.intersection(neighbors[sourceNode], targer).length > 0) {
-				res = true;
+	var oldAlgorithm = function(friends, delta) {
+		var isGood = function(users, n, c, delta) {
+			if(_.intersection(n, c).length < (1 - delta) * c.length)
 				return false;
-			}
-		});
 
-		return res;
+	        return _.intersection(n, _.difference(users, c)).length <= delta * c.length;
+    	};
+
+    	var used = [];
+	    var groups = {name: "Пользователь", children: []};
+	    var index = 0;
+	    var users = $.map(friends, function(friend) { return friend.uid; });
+	    while(users && users.length > 0 && index < users.length) {
+	      var v = users[index];
+	      var f = friends.find(function(element, index, array) { return element.uid == v; }).friends || [];
+	      var a = $.grep(f, function(id){
+	      	return !_.contains(used, id);
+	      });
+	      a.push(v);
+	      var removed = true;
+	      while (removed) {
+	        removed = false;
+	        $.each(a, function(index, x) {
+	        	var fr = friends.find(function(element, index, array) { return element.uid == x; }).friends || [];
+	        	var n = $.grep(fr, function(id) {
+	        		return !_.contains(used, id);
+	        	});
+	        	n.push(x);
+	        	if (!isGood(users, n, a, 3*delta)) {
+	        		var i = a.indexOf(x);
+	            	if (i > -1) {
+	              		a.splice(i, 1);
+	            	}
+
+		            removed = true;
+		            return false;
+	          	}
+	        });
+	      }
+
+	      $.each(users, function(index, user) {
+	        if (_.contains(a, user))
+	          return true;
+
+			var fr = friends.find(function(element, index, array) { return element.uid == user; }).friends || [];
+	        var n = $.grep(fr, function(id) {
+	        	return !_.contains(used, id);
+	        });
+
+	        n.push(user);
+	        if (isGood(users, n, a, 7*delta))
+	          a.push(user);
+	      });
+
+	      if (a && a.length > 0) {
+	        var group = {};
+	        group.name = a.length;	        
+	        group.children = $.map(a, function(id) {
+	          var i = users.indexOf(id);
+	            if (i > -1) {
+	              users.splice(i, 1);
+	              used.push(id);
+	            }
+	          var user = friends.find(function(element, index, array) { return element.uid == id; });
+	          user.name = (user.last_name || '' ) + ' ' + (user.first_name || '');
+	          user.size = 100;
+	          return user;
+	        });
+	        groups.children.push(group);
+	        group.size = a.length;
+	        index = 0;
+	      }
+	      else {
+	        index++;
+	      }
+	    }
+
+	    return groups;
 	};
 
 	return {
-		findCommunity: findCommunity,
+		oldAlgorithm: oldAlgorithm
 	}
 }());
